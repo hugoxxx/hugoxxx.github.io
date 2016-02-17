8d1dc65746a3e9b5ebc80c8e9552a8dda97572dd
Hugo Rocha <hugogr@gmail.com>
2016-02-17 16:32:37 -0200

    V2.2.4
    Detalhes

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,11 @@
-/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
-(function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
-
-/**
- * How to create a parallax scrolling website
- * Author: Petr Tichy
- * URL: www.ihatetomatoes.net
- * Article URL: http://ihatetomatoes.net/how-to-create-a-parallax-scrolling-website/
+ /**
+ * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
+ * Want to see more similar demos and tutorials?
+ * Help by spreading the word about Ihatetomatoes blog.
+ * Facebook - https://www.facebook.com/ihatetomatoesblog
+ * Twitter - https://twitter.com/ihatetomatoes
+ * Google+ - https://plus.google.com/u/0/109859280204979591787/about
+ * Article URL: http://ihatetomatoes.net/how-to-make-parallax-website-responsive/
  */
 
 ( function( $ ) {
@@ -13,8 +13,6 @@
 	// Setup variables
 	$window = $(window);
 	$slide = $('.homeSlide');
-	$slideTall = $('.homeSlideTall');
-	$slideTall2 = $('.homeSlideTall2');
 	$body = $('body');
 	
     //FadeIn all sections   
@@ -31,32 +29,58 @@
 	});
 	
 	function adjustWindow(){
-		
-		// Init Skrollr
-		var s = skrollr.init({
-		    render: function(data) {
-		    
-		        //Debugging - Log the current scroll position.
-		        //console.log(data.curTop);
-		    }
-		});
-		
-		// Get window size
+
+	    // Get window size
 	    winH = $window.height();
-	    
+	    winW = $window.width();
+
 	    // Keep minimum height 550
 	    if(winH <= 550) {
-			winH = 550;
-		} 
-	    
-	    // Resize our slides
-	    $slide.height(winH);
-	    $slideTall.height(winH*2);
-	    $slideTall2.height(winH*3);
-	    
-	    // Refresh Skrollr after resizing our sections
-	    s.refresh($('.homeSlide'));
-	    
+	        winH = 550;
+	    }
+
+	    // Init Skrollr for 768 and up
+	    if( winW >= 768) {
+
+	        // Init Skrollr
+	        var s = skrollr.init({
+	            forceHeight: false
+	        });
+
+	        // Resize our slides
+	        $slide.height(winH);
+
+	        s.refresh($('.homeSlide'));
+
+	    } else {
+
+	        // Init Skrollr
+	        var s = skrollr.init();
+	        s.destroy();
+	    }
+	
+		// Check for touch
+	   	if(Modernizr.touch) {
+
+			// Init Skrollr
+			var s = skrollr.init();
+			s.destroy();
+	   	}
+
 	}
+	
+	function initAdjustWindow() {
+	    return {
+	        match : function() {
+	            adjustWindow();
+	        },
+	        unmatch : function() {
+	            adjustWindow();
+	        }
+	    };
+	}
+
+	enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false)
+	        .listen(100);
 		
 } )( jQuery );
