6f1a3b3e99b055cd4d1d513fba824c9829d41d7a
Hugo Rocha <hugogr@gmail.com>
2016-02-19 03:06:46 -0200

    V2.3.6

--- js/loading.js
+++ js/loading.js
@@ -1,86 +1,98 @@
- /**
- * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
- * Want to see more similar demos and tutorials?
- * Help by spreading the word about Ihatetomatoes blog.
- * Facebook - https://www.facebook.com/ihatetomatoesblog
- * Twitter - https://twitter.com/ihatetomatoes
- * Google+ - https://plus.google.com/u/0/109859280204979591787/about
- * Article URL: http://ihatetomatoes.net/how-to-make-parallax-website-responsive/
- */
-
-( function( $ ) {
-	
-	// Setup variables
-	$window = $(window);
-	$slide = $('.homeSlide');
-	$body = $('body');
-	
-    //FadeIn all sections   
-	$body.imagesLoaded( function() {
-		setTimeout(function() {
-		      
-		      // Resize sections
-		      adjustWindow();
-		      
-		      // Fade in sections
-			  $body.removeClass('loading').addClass('loaded');
-			  
-		}, 800);
-	});
-	
-	function adjustWindow(){
-
-	    // Get window size
-	    winH = $window.height();
-	    winW = $window.width();
-
-	    // Keep minimum height 550
-	    if(winH <= 550) {
-	        winH = 550;
-	    }
-
-	    // Init Skrollr for 768 and up
-	    if( winW >= 768) {
-
-	        // Init Skrollr
-	        var s = skrollr.init({
-	            forceHeight: false
-	        });
-
-	        // Resize our slides
-	        $slide.height(winH);
-
-	        s.refresh($('.homeSlide'));
-
-	    } else {
-
-	        // Init Skrollr
-	        var s = skrollr.init();
-	        s.destroy();
-	    }
-	
-		// Check for touch
-	   	if(Modernizr.touch) {
-
-			// Init Skrollr
-			var s = skrollr.init();
-			s.destroy();
-	   	}
-
-	}
-	
-	function initAdjustWindow() {
-	    return {
-	        match : function() {
-	            adjustWindow();
-	        },
-	        unmatch : function() {
-	            adjustWindow();
-	        }
-	    };
-	}
-
-	enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false)
-	        .listen(100);
-		
-} )( jQuery );
+(function() {
+  (function($) {
+    return $.fn.imgPreload = function(options) {
+      var delay_completion, i, image_stack, placeholder_stack, replace, settings, spinner_stack, src, x, _i, _len;
+      settings = {
+        fake_delay: 10,
+        animation_duration: 1000,
+        spinner_src: 'spinner.gif'
+      };
+      if (options) {
+        $.extend(settings, options);
+      }
+      image_stack = [];
+      placeholder_stack = [];
+      spinner_stack = [];
+      window.delay_completed = false;
+      delay_completion = function() {
+        var x, _i, _len, _results;
+        window.delay_completed = true;
+        _results = [];
+        for (_i = 0, _len = image_stack.length; _i < _len; _i++) {
+          x = image_stack[_i];
+          _results.push($(x).attr('data-load-after-delay') === 'true' ? (replace(x), $(x).removeAttr('data-load-after-delay')) : void 0);
+        }
+        return _results;
+      };
+      setTimeout(delay_completion, settings.fake_delay);
+      this.each(function() {
+        var $image, $placeholder, $spinner_img, offset_left, offset_top;
+        $image = $(this);
+        offset_top = $image.offset().top;
+        offset_left = $image.offset().left;
+        $spinner_img = $('<img>');
+        $placeholder = $('<img>').attr({
+          src: 'data:image/gif;base64,R0lGODlhAQABA\
+                IABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
+        });
+        $placeholder.attr({
+          width: $image.attr('width')
+        });
+        $placeholder.attr({
+          height: $image.attr('height')
+        });
+        spinner_stack.push($spinner_img);
+        placeholder_stack.push($placeholder);
+        image_stack.push($image.replaceWith($placeholder));
+        $('body').append($spinner_img);
+        $spinner_img.css({
+          position: 'absolute'
+        });
+        $spinner_img.css('z-index', 9999);
+        $spinner_img.load(function() {
+          $(this).css({
+            left: (offset_left + $placeholder.width() / 2) - $(this).width() / 2
+          });
+          return $(this).css({
+            top: (offset_top + $placeholder.height() / 2) - $(this).height() / 2
+          });
+        });
+        return $spinner_img.attr({
+          src: settings.spinner_src
+        });
+      });
+      i = 0;
+      for (_i = 0, _len = image_stack.length; _i < _len; _i++) {
+        x = image_stack[_i];
+        x.attr({
+          no: i++
+        });
+        src = x.attr('src');
+        x.attr({
+          src: ''
+        });
+        x.load(function() {
+          if (window.delay_completed) {
+            return replace(this);
+          } else {
+            return $(this).attr('data-load-after-delay', true);
+          }
+        });
+        x.attr({
+          src: src
+        });
+      }
+      replace = function(image) {
+        var $image, no_;
+        $image = $(image);
+        no_ = $image.attr('no');
+        placeholder_stack[no_].replaceWith($image);
+        spinner_stack[no_].fadeOut(settings.animation_duration / 2, function() {
+          return $(this).remove();
+        });
+        return $image.fadeOut(0).fadeIn(settings.animation_duration);
+      };
+      return this;
+    };
+  })(jQuery);
+}).call(this);
